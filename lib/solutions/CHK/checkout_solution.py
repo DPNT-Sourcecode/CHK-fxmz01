48664fef16032d998bb1b1cf080e0617a36206fb
sbx_user1051 <sbx_user1051@169.254.221.13>
2023-07-08 18:12:56 +0000

    Sat Jul 08 18:12:56 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,15 @@
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    for sku in skus.split():
+        
+    return -1
+
--- test/solution_tests/CHK/test_sum.py
+++ test/solution_tests/CHK/test_sum.py
@@ -1,20 +1,30 @@
-
-
-
 from lib.solutions.CHK.checkout_solution import checkout
 
 
-class TestSum:
+def test_empty():
+    assert checkout("") == -1
 
-    def test_empty(self):
-        assert checkout("") == -1
-        
-    def test_badtype_input():
-        assert checkout(1) == -1
 
-    def test_singleitems(self):
-        assert checkout("A") == 50
-        assert checkout("B") == 30
-        assert checkout("C") == 20
+def test_badtype_input():
+    assert checkout(1) == -1
 
-    def test_
+
+def test_non_existing_sku():
+    assert checkout("X") == -1
+    assert checkout("A X") == -1
+
+
+def test_singleitems():
+    assert checkout("A") == 50
+    assert checkout("B") == 30
+    assert checkout("C") == 20
+
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
