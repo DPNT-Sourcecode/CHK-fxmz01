a33fb6c325f12aa322851b86fa6bb3296f92e70a
sbx_user1051 <sbx_user1051@169.254.24.121>
2023-07-08 21:12:18 +0000

    Sat Jul 08 21:12:18 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,24 +10,36 @@ prices = {
     "D": 15,
 }
 
+discounts = {
+    # By hardcoding discounts ordering (big to small) we can simplify implementation.
+    # This of course wouldn't apply in a real case where this data would come from a DB
+    "A": [(5, 200), (3, 130)],
+    "B": [(2, 45)],
+}
 
-class Discounts:
-    discounts = {
-        # By hardcoding discounts ordering (big to small) we can simplify implementation.
-        # This of course wouldn't apply in a real case where this data would come from a DB
-        "A": [(5, 200), (3, 130)],
-        "B": [(2, 45)],
-    }
+# class Discounts:
+#     discounts = {
+#         # By hardcoding discounts ordering (big to small) we can simplify implementation.
+#         # This of course wouldn't apply in a real case where this data would come from a DB
+#         "A": [(5, 200), (3, 130)],
+#         "B": [(2, 45)],
+#     }
 
-    @classmethod
-    def get(cls, sku: str):
-        d = cls.discounts.get(sku)
-        if d:
-            return d[0]
-        else:
-            return None
+#     @classmethod
+#     def get(cls, sku: str):
+#         d = cls.discounts.get(sku)
+#         if d:
+#             return d[0]
+#         else:
+#             return None
 
-
+def items_prize(sku, count):
+    prize = 0
+    for pack_size, pack_prize in discounts.get(sku, []):
+        
+def _discountpack_counts(count: int, pack_size: int) -> tuple[int, int]:
+    """Returns number of packs, number of individual priced items"""
+    return count // pack_size, count % pack_size
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -55,7 +67,6 @@ def _counter(skus: str) -> dict[str, int]:
     return Counter(skus.replace(" ", ""))
 
 
-def _discountpack_counts(count: int, pack_size: int) -> tuple[int, int]:
-    """Returns number of packs, number of individual priced items"""
-    return count // pack_size, count % pack_size
+
+
 
