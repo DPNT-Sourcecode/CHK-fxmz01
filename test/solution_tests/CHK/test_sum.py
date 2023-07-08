64d7c3e57f62bbb836c30a4512b99fc3e6d6d869
sbx_user1051 <sbx_user1051@169.254.221.13>
2023-07-08 18:09:56 +0000

    Sat Jul 08 18:09:56 UTC 2023

--- test/solution_tests/CHK/test_sum.py
+++ test/solution_tests/CHK/test_sum.py
@@ -1,7 +1,20 @@
-from lib.solutions.CHK import checkout_solution
+
+
+
+from lib.solutions.CHK.checkout_solution import checkout
 
 
 class TestSum:
+
+    def test_empty(self):
+        assert checkout("") == -1
+        
+    def test_badtype_input():
+        assert checkout(1) == -1
+
     def test_singleitems(self):
-        assert checkout_solution.checkout(["A"]) == 50
-        assert checkout_solution.checkout(["A"]) == 50
+        assert checkout("A") == 50
+        assert checkout("B") == 30
+        assert checkout("C") == 20
+
+    def test_
