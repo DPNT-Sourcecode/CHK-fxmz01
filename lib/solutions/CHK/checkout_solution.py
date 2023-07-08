edc2e2b1edbe338af5b32d22afab9f6516bedea8
sbx_user1051 <sbx_user1051@169.254.221.13>
2023-07-08 18:39:56 +0000

    Sat Jul 08 18:39:56 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,17 +23,23 @@ def checkout(skus: str) -> int:
         return ERROR
 
     total = 0
-    
+    for sku, count in _counter(skus).items():
+        print(sku)
+        print(count)
+
     for sku in skus.split():
         if sku not in prices:
             return ERROR
         total += prices[sku]
     return total
 
+
 def _counter(skus: str) -> dict[str, int]:
     return Counter(skus.split())
+
 
 def _discountpacks(total: int, pack_size) -> tuple[int, int]:
     """Returns number of packs, number of individual priced items"""
     return total // pack_size, total % pack_size
+
 
